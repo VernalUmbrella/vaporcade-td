5ad3eff717c6bfa17faa5e5a3de241f694916437
VernalUmbrella <vernalumbrella@gmail.com>
2024-10-12 17:17:53 -0400

    Add highlight to currently active tower

--- ui/hud/tower_button.gd
+++ ui/hud/tower_button.gd
@@ -3,9 +3,21 @@ extends TextureButton
 
 @export var tower_stats: TowerStats
 
+@onready var highlight: TextureRect = $Highlight
+
+var active: bool = false:
+	set(value):
+		active = value
+		button_pressed = active
+		highlight.visible = active
+
 func _ready() -> void:
 	assert(tower_stats.texture)
 	texture_normal = tower_stats.texture
+	Events.tower_selected.connect(_on_tower_selected)
 
 func _pressed() -> void:
-	Events.tower_selected.emit(tower_stats)
+	Events.tower_selected.emit(null if active else tower_stats)
+
+func _on_tower_selected(which: TowerStats):
+	active = (which == tower_stats)
